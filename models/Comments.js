cd1682e3d08cbdb75dea01ab4ecacfedffd35560
Justin Joyce <jjoyce07@yahoo.com>
2015-11-30 11:10:55 -0600

    adding downvotes

--- models/Comments.js
+++ models/Comments.js
@@ -4,6 +4,7 @@ var CommentSchema = new mongoose.Schema({
   body: String,
   author: String,
   upvotes: {type: Number, default: 0},
+  downvotes: {type: Number, default: 0},
   post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
 });
 
